fa417b313d0fe160d7b5836a102e0b6f6220194b
Max Base <MaxBaseCode@Gmail.Com>
2023-04-09 18:49:26 +0100

    update regex

---
+++ url-validator.js
@@ -0,0 +1,31 @@
+// Regex to validate url and links (even without http or https)
+const REGEX = /((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi;
+const EXACT_REGEX = /^((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)$/gi;
+
+// Return an array from all URL address inside the input text
+const match = (text) => {
+    return text.match(REGEX);
+};
+
+// Return true if the input text has at least one URL address
+const has = (text) => {
+    return REGEX.test(text);
+};
+
+// Return true if the input text includes the URL address
+const includes = (text, email) => {
+    return match(text).includes(email);
+};
+
+// Return true if the input text match only one URL address without any other characters or whitespaces
+const exact = (text) => {
+    return EXACT_REGEX.test(text);
+};
+
+// Export the functions
+module.exports = {
+    match,
+    includes,
+    has,
+    exact
+};
